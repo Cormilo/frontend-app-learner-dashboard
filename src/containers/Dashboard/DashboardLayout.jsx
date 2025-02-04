09a176ed77470825d7c1a29c1a3ca061f9982c94
Cormilo <gitgitgithub@yandex.ru>
2025-02-04 23:20:05 +0300

    удаляем sidebar-column

--- src/containers/Dashboard/DashboardLayout.jsx
+++ src/containers/Dashboard/DashboardLayout.jsx
@@ -40,10 +40,7 @@ export const DashboardLayout = ({ children }) => {
         <Col {...courseListColumnProps} className="course-list-column">
           {children}
         </Col>
-        <Col {...columnConfig.sidebar} className="sidebar-column">
-          {!isCollapsed && (<h2 className="course-list-title">&nbsp;</h2>)}
-          <WidgetSidebarSlot />
-        </Col>
+
       </Row>
     </Container>
   );
